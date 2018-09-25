c6d2d254d2fb65f66c1e2cac2dd537128f00e8e1
rahlk <i.m.ralk@gmail.com>
2018-09-24 22:42:47 -0400

    Autocommit from makefile

--- src/metrics_getter.py
+++ src/metrics_getter.py
@@ -1,4 +1,5 @@
 import os
+import re
 import shlex
 import numpy as np
 import pandas as pd
@@ -8,7 +9,6 @@ import subprocess as sp
 import understand as und
 from pathlib import Path
 from pdb import set_trace
-from bs4 import BeautifulSoup
 from collections import defaultdict
 
 REPO_LINKS = {
@@ -41,33 +41,100 @@ class MetricsGetter:
         -----
         Save the current working directory and cd to source directory.
         """
+        # Reference current directory, so we can go back after we are done.
         self.cwd = Path(os.getcwd())
 
         # Create path to datasets
         self.datasets_path = self.cwd.joinpath("datasets", self.project_name)
 
+        # Create a folder with the project name for the datasets
         if not self.datasets_path.is_dir():
-            # Create a folder with the project name for the datasets
             os.makedirs(self.datasets_path)
 
-        # Generate clone path
-        self.source_path = self.cwd.joinpath("sources", self.project_name)
-        if not self.datasets_path.is_dir():
-            cmd = shlex.split("git clone {} {}".format(REPO_LINKS[
+        # Generate path to store udb files
+        self.udb_path = self.cwd.joinpath(".temp", "udb")
+
+        # Create a folder to hold the udb files
+        if not self.udb_path.is_dir():
+            os.makedirs(self.udb_path)
+        
+        # Create a handle for storing *.udb file for the project 
+        self.und_file = self.udb_path.joinpath("{}.udb".format(self.project_name))
+        
+        # Generate source path where the source file exist
+        self.source_path = self.cwd.joinpath(".temp", "sources", self.project_name)
+        
+        # If project source doesn't exist, clone it.
+        if not self.source_path.is_dir():
+            self._os_cmd("git clone {} {}".format(REPO_LINKS[
                 self.project_name], self.source_path))
-            sp.call(cmd)
-        os.chdir(self.source_path)
+        
+        # Go to the udb path
+        os.chdir(self.udb_path)
+
+        # If the und file doesn't exist, create it.        
+        if not self.und_file.is_file():
+            # Generate udb file
+            cmd = "und create -languages python C++ Fortran add {} analyze {}".format(
+                str(self.source_path), str(self.und_file))
+            self._os_cmd(cmd)
+
+        # Go to the cloned repo
+        os.chdir(self.udb_path)
+        
         return self
+
+    @staticmethod
+    def _os_cmd(cmd):
+        """
+        Run a command on the shell
+        
+        Parameters
+        ----------
+        cmd: str
+            A command to run.
+        """
+        cmd = shlex.split(cmd)
+        with sp.Popen(cmd, stdin=sp.PIPE, stdout=sp.PIPE) as p:
+            out, err = p.communicate()
+        return out, err
 
     def generate_understand_reports(self):
         """
         Use the understand tool's API to generate metrics
-        """
-        set_trace()
 
-        # cmd = shlex.split(
-        # "und -db {}.udb create -languages python C++ Fortran add {} analyze report".format(und_filename, sources_path))
-        # sp.call(cmd)
+        Notes
+        -----
+        
+        """
+        db = und.open(str(self.und_file))
+        cplusplus_metrics = und.Metric.list('c')
+        python_metrics = und.Metric.list('Python')
+        fortran_metrics = und.Metric.list('Fortran')
+        # -----------------------------------------------------------------------
+        # ---------- FORTRAN METRICS --------------------------------------------
+        # -----------------------------------------------------------------------
+        searchstr = re.compile(".*\.F90", re.I)
+        f90_metrics = defaultdict(list)
+        for file in db.lookup(searchstr, "File"):
+            set_trace()
+
+        # -----------------------------------------------------------------------
+        # ----------  PYTHON METRICS --------------------------------------------
+        # -----------------------------------------------------------------------
+        py_metrics = defaultdict(list)
+        searchstr = re.compile(".*\.py", re.I)
+        for file in db.lookup(searchstr, "File"):
+            set_trace()
+        
+        # -----------------------------------------------------------------------
+        # ----------    C and C++    --------------------------------------------
+        # -----------------------------------------------------------------------
+        c_metrics = defaultdict(list)
+        searchstr = re.compile(".*\.c", re.I)
+        for file in db.lookup(searchstr, "File"):
+            set_trace()
+
 
     def __exit__(self, exception_type, exception_value, traceback):
         """
@@ -79,7 +146,7 @@ class MetricsGetter:
         """
 
         os.chdir('..')
-        os.removedirs(self.project_name)
+        self._os_cmd("rm -rf {}".format(self.source_path))
         os.chdir(self.cwd)
 
 
