92ecfb24bfa03aa33275fda85c1b162a85dfee6e
rahlk <i.m.ralk@gmail.com>
2018-10-30 14:33:03 -0400

    Edits to process data

--- datasets/process_data.py
+++ datasets/process_data.py
@@ -1,16 +1,18 @@
 import pandas as pd
 import numpy as np
 from pdb import set_trace
+from sklearn.model_selection import StratifiedKFold
 
 
 def chunk_data():
-    dframe = pd.read_csv("abinit.csv")
-    set_trace()
-    chunks = np.array_split(dframe, indices_or_sections=50)
-    for i, chunk in enumerate(chunks):
-        chunk = chunk.set_index(keys=chunk.columns[0])
-        chunk.to_csv("abinit/abinit_{}.csv".format(i+1), index=True)
-
-
+    skf = StratifiedKFold(n_splits = 10, shuffle=True)
+    dframe = pd.read_csv("mdanalysis.csv")
+    X = dframe[dframe.columns[:-1]]
+    y = dframe[dframe.columns[-1]]
+    
+    for i, (train_index, test_index) in enumerate(skf.split(X, y)):
+        dframe.ix[test_index].to_csv(
+            "mdanalysis/mdanalysis_{}.csv".format(i+1), index=False)
+        
 if __name__ == "__main__":
     chunk_data()
